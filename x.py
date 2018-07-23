fc7582f322db3c2f9a3339dda14fae4f24fd0bc2
stillwilliam <36806948+stillwilliam@users.noreply.github.com>
2018-07-23 10:08:29 +0800

    Update x.py

--- x.py
+++ x.py
@@ -1,6 +1,6 @@
 # -*-coding: utf-8 -*-
 
-from Linphu.linepy import *
+from Linephu.linepy import *
 from datetime import datetime
 from time import sleep
 from humanfriendly import format_timespan, format_size, format_number, format_length
